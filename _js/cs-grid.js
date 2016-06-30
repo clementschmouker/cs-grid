71936c544f4b88fd57ccaf27801b9ac9fe80b9f1
Clément Schmouker <clement.schmouker@gmail.com>
2016-06-30 18:09:00 +0200

    Still a bug in certain case
    Don't know yet if this is due to gridwidth beeing to high, or column
    number beeing to high

--- _js/cs-grid.js
+++ _js/cs-grid.js
@@ -10,20 +10,25 @@ var DOMGridContainer = document.createElement("div");
 DOMGridContainer.classList.add("cs-gridContainer");
 DOMBody.appendChild(DOMGridContainer);
 
-DOMGridContainer.style.width = iGridWidth+"px";
 
 var DOMGridColumns = [];
 var DOMGridGutters = [];
-for( var i = 0; i < iGridColumns; i += 1) {
-  DOMGridGutters[i] = document.createElement("div");
-  DOMGridGutters[i].classList.add("cs-gridGutter");
-  DOMGridGutters[i].style.width = iGridGutterWidth+"px";
 
-  DOMGridColumns[i] = document.createElement("div");
-  DOMGridColumns[i].classList.add("cs-gridColumn")
-  DOMGridColumns[i].style.width = ((iGridWidth / iGridColumns) - iGridGutterWidth) + iGridGutterWidth/iGridColumns+"px";
-  DOMGridContainer.appendChild(DOMGridColumns[i]);
-  if(i != iGridColumns-1){ //in order to not add a gutter to the end
-    DOMGridContainer.appendChild(DOMGridGutters[i]);
+function setCSGrid( gridwidth, gridcolumnnumber, gridgutterwidth) {
+  DOMGridContainer.style.width = gridwidth+"px";
+  for( var i = 0; i < gridcolumnnumber; i += 1) {
+    DOMGridGutters[i] = document.createElement("div");
+    DOMGridGutters[i].classList.add("cs-gridGutter");
+    DOMGridGutters[i].style.width = gridgutterwidth+"px";
+
+    DOMGridColumns[i] = document.createElement("div");
+    DOMGridColumns[i].classList.add("cs-gridColumn")
+    DOMGridColumns[i].style.width = ((gridwidth / gridcolumnnumber) - gridgutterwidth) + (gridgutterwidth/gridcolumnnumber)+"px";
+    DOMGridContainer.appendChild(DOMGridColumns[i]);
+    if(i != gridcolumnnumber-1){ //in order to not add a gutter to the end
+      DOMGridContainer.appendChild(DOMGridGutters[i]);
+    }
   }
 }
+
+setCSGrid(1000, 6, 20);

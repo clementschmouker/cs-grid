b72396cfd3d43bd24d23cf20f693fffc9fd61d89
Clément Schmouker <clement.schmouker@gmail.com>
2016-07-01 01:49:29 +0200

    Refactored into an object grid

--- _js/cs-grid.js
+++ _js/cs-grid.js
@@ -1,66 +1,72 @@
+var CSGrid = function() {
+  this.DOMGridContainer = 0;
+  this.DOMGridColumns = [];
+  this.DOMGridGutters = [];
+  this.DOMGridRows = [];
+  this.bActive = true;
+}
+
+
 //Global Variables
-var DOMBody = document.getElementsByTagName("body")[0];
-
-var DOMGridContainer = document.createElement("div");
-DOMGridContainer.classList.add("cs-gridContainer");
-
-
-var DOMGridColumns = [];
-var DOMGridGutters = [];
-var DOMGridRows = [];
 
 var DOMGridButton = document.createElement("button");
-DOMGridButton.classList.add("cs-gridButton");
-DOMGridButton.innerHTML = "ON";
-var bActive = true;
+var csGrid = new CSGrid();
 
 function setCSGrid( gridwidth, gridcolumnnumber, gridgutterwidth, gridrowheight, activategrid) {
-  DOMBody.appendChild(DOMGridContainer);
-  DOMBody.appendChild(DOMGridButton);
-  DOMGridContainer.style.width = gridwidth+"px";
-  for( var i = 0; i < gridcolumnnumber; i += 1) {
-    DOMGridGutters[i] = document.createElement("div");
-    DOMGridGutters[i].classList.add("cs-gridGutter");
-    DOMGridGutters[i].style.width = gridgutterwidth+"px";
 
-    DOMGridColumns[i] = document.createElement("div");
-    DOMGridColumns[i].classList.add("cs-gridColumn")
-    DOMGridColumns[i].style.width = ((gridwidth / gridcolumnnumber) - gridgutterwidth) + (gridgutterwidth/gridcolumnnumber)+"px";
-    DOMGridContainer.appendChild(DOMGridColumns[i]);
+  csGrid.DOMGridContainer = document.createElement("div");
+  csGrid.DOMGridContainer.classList.add("cs-gridContainer");
+
+
+  DOMGridButton.classList.add("cs-gridButton");
+  DOMGridButton.innerHTML = "ON";
+
+  document.getElementsByTagName("body")[0].appendChild(csGrid.DOMGridContainer);
+  document.getElementsByTagName("body")[0].appendChild(DOMGridButton);
+  csGrid.DOMGridContainer.style.width = gridwidth+"px";
+  for( var i = 0; i < gridcolumnnumber; i += 1) {
+    csGrid.DOMGridGutters[i] = document.createElement("div");
+    csGrid.DOMGridGutters[i].classList.add("cs-gridGutter");
+    csGrid.DOMGridGutters[i].style.width = gridgutterwidth+"px";
+
+    csGrid.DOMGridColumns[i] = document.createElement("div");
+    csGrid.DOMGridColumns[i].classList.add("cs-gridColumn")
+    csGrid.DOMGridColumns[i].style.width = ((gridwidth / gridcolumnnumber) - gridgutterwidth) + (gridgutterwidth/gridcolumnnumber)+"px";
+    csGrid.DOMGridContainer.appendChild(csGrid.DOMGridColumns[i]);
     if(i != gridcolumnnumber-1){ //in order to not add a gutter to the end
-      DOMGridContainer.appendChild(DOMGridGutters[i]);
+      csGrid.DOMGridContainer.appendChild(csGrid.DOMGridGutters[i]);
     }
   }
   //Checks if there needs to be rows
   if(gridrowheight){
     for(var i = 0; i < document.body.offsetHeight / gridrowheight - 1; i += 1) {
-      DOMGridRows[i] = document.createElement("div");
-      DOMGridRows[i].classList.add("cs-gridRow");
-      DOMGridRows[i].style.height = gridrowheight+"px";
-      DOMGridContainer.appendChild(DOMGridRows[i]);
+      csGrid.DOMGridRows[i] = document.createElement("div");
+      csGrid.DOMGridRows[i].classList.add("cs-gridRow");
+      csGrid.DOMGridRows[i].style.height = gridrowheight+"px";
+      csGrid.DOMGridContainer.appendChild(csGrid.DOMGridRows[i]);
     }
   }
   //Checks if there needs to start active/inactive
-  bActive = activategrid;
-  if(bActive == true) {
-    DOMGridContainer.style.opacity = 1;
+  csGrid.bActive = activategrid;
+  if(csGrid.bActive == true) {
+    csGrid.DOMGridContainer.style.opacity = 1;
     DOMGridButton.innerHTML = "ON";
   }
-  if(bActive == false) {
-    DOMGridContainer.style.opacity = 0;
+  if(csGrid.bActive == false) {
+    csGrid.DOMGridContainer.style.opacity = 0;
     DOMGridButton.innerHTML = "OFF";
   }
 }
 
 DOMGridButton.addEventListener("click", function() {
-  if(bActive == false) {
-    bActive = true;
-    DOMGridContainer.style.opacity = 1;
+  if(csGrid.bActive == false) {
+    csGrid.bActive = true;
+    csGrid.DOMGridContainer.style.opacity = 1;
     DOMGridButton.innerHTML = "ON";
   }
   else {
-    bActive = false;
-    DOMGridContainer.style.opacity = 0;
+    csGrid.bActive = false;
+    csGrid.DOMGridContainer.style.opacity = 0;
     DOMGridButton.innerHTML = "OFF";
   }
 });

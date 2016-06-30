3b033c8d349b4d2cf7e7ad5667b72d8cb8edfeaf
Clément Schmouker <clement.schmouker@gmail.com>
2016-06-30 17:29:40 +0200

    Created columns

--- _js/cs-grid.js
+++ _js/cs-grid.js
@@ -2,12 +2,19 @@
 var DOMBody = document.getElementsByTagName("body")[0];
 
 var iGridWidth = 900;
-var iGridColumns = 0;
+var iGridColumns = 2;
 var iGutterWidth = 0;
 
 //Create Grid Container
 var DOMGridContainer = document.createElement("div");
-DOMGridContainer.classList.add("gridContainer");
+DOMGridContainer.classList.add("cs-gridContainer");
 DOMBody.appendChild(DOMGridContainer);
 
 DOMGridContainer.style.width = iGridWidth+"px";
+
+var DOMGridColumns = [];
+for( var i = 0; i < iGridColumns; i += 1) {
+  DOMGridColumns[i] = document.createElement("div");
+  DOMGridColumns[i].classList.add("cs-gridColumn")
+  DOMGridContainer.appendChild(DOMGridColumns[i]);
+}
